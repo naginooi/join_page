f975712cbc7fa0c8583f2e51624d0e2029e615a5
naginooi <xxxkmsz@naver.com>
2023-09-11 17:00:37 +0900

    token event

--- Register/final.js
+++ Register/final.js
@@ -1,3 +1,4 @@
+//////// focus 이동 /////////////////////////////////////
 const firstPhone = () => {
   const phone1 = document.getElementById("first-input").value;
   //next
@@ -9,6 +10,17 @@ const secondPhone = () => {
   if (phone2.length === 4) document.getElementById("third-input").focus();
 };
 const thirdPhone = () => {
+  const phone1 = document.getElementById("first-input").value;
+  const phone2 = document.getElementById("second-input").value;
   const phone3 = document.getElementById("third-input").value;
-  document.querySelector(".sendBtn").removeAttribute("disabled");
+  if (phone1.length === 3 && phone2.length === 4 && phone3.length === 4)
+    document.querySelector(".sendBtn").removeAttribute("disabled");
+};
+
+/////////////////////////////////////////////////////////////////////////
+
+///////token 생성//////////////////////////////////////////
+const makeToken = () => {
+  const token = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+  document.querySelector(".token").innerText = token;
 };
