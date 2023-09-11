27747eac366377a9a9a6195c698eb6ee7418a389
naginooi <xxxkmsz@naver.com>
2023-09-11 16:42:37 +0900

    focus event

--- Register/final.js
+++ Register/final.js
@@ -1,3 +1,14 @@
-// document.querySelector('.register').addEventListener('click',e=>{
-
-// })
+const firstPhone = () => {
+  const phone1 = document.getElementById("first-input").value;
+  //next
+  if (phone1.length === 3) document.getElementById("second-input").focus();
+};
+const secondPhone = () => {
+  const phone2 = document.getElementById("second-input").value;
+  //next
+  if (phone2.length === 4) document.getElementById("third-input").focus();
+};
+const thirdPhone = () => {
+  const phone3 = document.getElementById("third-input").value;
+  document.querySelector(".sendBtn").removeAttribute("disabled");
+};
